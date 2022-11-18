5274593706c5681be4b40277097a5285429e89de
kwinso <ruslan.sot70@gmail.com>
2022-11-18 13:25:02 +0300

    Fixing some bugs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// This file would not be possible without Nekear
+
 use chrono::{DateTime, Datelike, Duration, Local, Timelike};
 use clap::Parser;
 use std::{
@@ -7,6 +9,7 @@ use std::{
     time::Instant,
 };
 use ureq;
+
 use tar::Builder as TarBuilder;
 
 #[derive(Parser, Debug)]
@@ -28,7 +31,7 @@ struct Args {
     proxy_url: Option<String>,
 
     /// Token of the bot which is used to send notifications
-    #[clap(short, long, required = false)]
+    #[clap(short, long = "token", required = false)]
     telegram_token: Option<String>,
 
     /// Id of the chat where to send a notification
@@ -50,20 +53,21 @@ fn main() {
         exit(1);
     }
 
+    let local_time = chrono::offset::Local::now();
     // Add -1 becuase of partition point algorithm finds index where it's "given date and less",
     // and we need strictly less than gived date
-    let archive_from = chrono::offset::Local::now() - Duration::days(args.archive as i64 - 1);
+    let archive_from = local_time - Duration::days(args.archive as i64 - 1);
     let archive_from = normalize_date(&archive_from);
 
-    let delete_from = chrono::offset::Local::now() - Duration::days(args.delete as i64);
+    let delete_from = local_time - Duration::days(args.delete as i64);
     let delete_from = normalize_date(&delete_from);
 
-    process_dir(&args.directory, &archive_from, &delete_from);
+    let processed = process_dir(&args.directory, &archive_from, &delete_from);
 
-    send_telegram_notification(&args, now.elapsed());
+    send_telegram_notification(&args, now.elapsed(), processed, delete_from, archive_from);
 }
 
-fn send_telegram_notification(args: &Args, elapsed_time: std::time::Duration) {
+fn send_telegram_notification(args: &Args, elapsed_time: std::time::Duration, amount: usize, from_date: DateTime<Local>, to_date: DateTime<Local>) {
     if args.telegram_token.is_none() {
         println!("No notificaion send since no telegram token provided.");
         return;
@@ -74,20 +78,34 @@ fn send_telegram_notification(args: &Args, elapsed_time: std::time::Duration) {
         return;
     }
 
+    let ip = get_host_ip();
+
+    if ip.is_none() {
+        println!("No valid host IP found to send notifcation to Telegram.");
+        return;
+    }
+
     let http = get_http_client(&args.proxy_url);
     if let Err(e) = &http {
         println!("Bad proxy configuration: {e}");
         return;
     }
 
+    let ip = ip.unwrap();
     let token = args.telegram_token.clone().unwrap();
     let chat_id = args.chat_id.clone().unwrap();
     let http = http.unwrap();
 
     // TODO: Add ip info
     let text = format!(
-        "Произведена очистка логов на `<IP>`\n*Время выполенения*: {:.2}s",
-        elapsed_time.as_secs_f32()
+        r#"Произведена очистка логов на `{ip}`
+Время выполенения: *{:.2}s*
+Файлов архивировано за период с `{}` по `{}`: *{} файлов*.
+"#,
+        elapsed_time.as_secs_f32(),
+        from_date.format("%d.%m.%Y"),
+        to_date.format("%d.%m.%Y"),
+        amount
     );
 
     let res = http
@@ -112,6 +130,20 @@ fn send_telegram_notification(args: &Args, elapsed_time: std::time::Duration) {
     }
 }
 
+fn get_host_ip() -> Option<String> {
+    let mut ip: Option<String> = None;
+
+    for adapter in ipconfig::get_adapters().expect("Failed to get own IP address") {
+        let addr = adapter.ip_addresses()[1].to_string();
+        if addr.starts_with("10.") {
+            ip = Some(addr);
+            break;
+        }
+    }
+
+    return ip;
+}
+
 fn get_http_client(proxy_url: &Option<String>) -> Result<ureq::Agent, ureq::Error> {
     let mut builder = ureq::AgentBuilder::new();
 
@@ -126,7 +158,7 @@ fn list_dir_files(path: &PathBuf) -> Vec<DirEntry> {
     return fs::read_dir(path)
         .unwrap()
         .map(|v| v.unwrap())
-        .filter(|v| v.path().is_file() && !v.file_name().to_string_lossy().ends_with(".tar"))
+        .filter(|v| v.path().is_file())
         .collect();
 }
 
@@ -152,7 +184,7 @@ fn is_same_day(a: &DateTime<Local>, b: &DateTime<Local>) -> bool {
     return a.year() == b.year() && a.month() == b.month() && a.day() == b.day();
 }
 
-fn archive_files(files: &Vec<&DirEntry>, dir: &PathBuf, date: &DateTime<Local>) {
+fn pack_to_archive(files: &Vec<&DirEntry>, dir: &PathBuf, date: &DateTime<Local>) {
     let human_readable = date.format("%d-%m-%Y");
     let dest = dir.join(format!(
         "{}_{}.tar",
@@ -173,11 +205,21 @@ fn archive_files(files: &Vec<&DirEntry>, dir: &PathBuf, date: &DateTime<Local>) 
     tar.finish().unwrap();
 }
 
-fn zip_files(files: &[DirEntry], parent_dir: &PathBuf) {
+fn archive_files(files: &[DirEntry], parent_dir: &PathBuf) -> usize {
+    if files.len() < 1 {
+        return 0;
+    }
+
     let mut current_date: DateTime<Local> = files[0].metadata().unwrap().modified().unwrap().into();
     let mut files_to_archive: Vec<&DirEntry> = vec![];
+    let mut amount = 0;
 
     for f in files.iter() {
+        // Skip archives
+        if f.file_name().to_string_lossy().ends_with(".tar") {
+            continue;
+        }
+
         let date: DateTime<Local> = f.metadata().unwrap().modified().unwrap().into();
 
         if is_same_day(&date, &current_date) {
@@ -185,7 +227,9 @@ fn zip_files(files: &[DirEntry], parent_dir: &PathBuf) {
             continue;
         }
 
-        archive_files(&files_to_archive, &parent_dir, &current_date);
+        amount += files_to_archive.len();
+
+        pack_to_archive(&files_to_archive, &parent_dir, &current_date);
         // Reset for the next date
         files_to_archive.clear();
         current_date = date.clone();
@@ -194,10 +238,14 @@ fn zip_files(files: &[DirEntry], parent_dir: &PathBuf) {
     }
 
     // Archive last date
-    archive_files(&files_to_archive, &parent_dir, &current_date);
+    amount += files_to_archive.len();
+    pack_to_archive(&files_to_archive, &parent_dir, &current_date);
+
+
+    return amount;
 }
 
-fn process_dir(dir: &PathBuf, archive_from: &DateTime<Local>, delete_from: &DateTime<Local>) {
+fn process_dir(dir: &PathBuf, archive_from: &DateTime<Local>, delete_from: &DateTime<Local>) -> usize {
     // Sort files by modified time
     let mut files = list_dir_files(dir);
     files.sort_by(|a, b| {
@@ -206,6 +254,8 @@ fn process_dir(dir: &PathBuf, archive_from: &DateTime<Local>, delete_from: &Date
 
         return a_upd.cmp(&b_upd);
     });
+
+    let mut processed = 0;
 
     if files.len() > 0 {
         // Find start and end indexes for chunk of data that should be proceesed
@@ -226,10 +276,12 @@ fn process_dir(dir: &PathBuf, archive_from: &DateTime<Local>, delete_from: &Date
             fs::remove_file(deleted.path()).unwrap();
         }
 
-        zip_files(&files[start..end], &dir);
+        processed = archive_files(&files[start..end], &dir);
     }
 
     for sub in list_subdirs(&dir) {
-        process_dir(&sub.path(), archive_from, delete_from);
+        processed += process_dir(&sub.path(), archive_from, delete_from);
     }
+
+    return processed;
 }
